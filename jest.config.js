6f270ac340aaf60e450ea5a8baeeb8a036ad22ca
Minor Gordon <minorg@server.fake>
2024-05-26 13:29:44 -0400

    fighting Jest, again

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const jestConfig = {
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  preset: "ts-jest/presets/default-esm", // or other ESM presets
+  testRegex: ".*\\.test\\.ts",
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
+
+export default jestConfig;
