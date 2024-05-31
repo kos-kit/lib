86ec730a63acee261e7f11041b2b1ec6ebb8e978
Minor Gordon <minorg@server.fake>
2024-05-31 17:31:02 -0400

    envalid validators

---
+++ src/utilities/envalidValidators.ts
@@ -0,0 +1,44 @@
+import * as envalid from "envalid";
+import { makeStructuredValidator } from "envalid/dist/makers";
+import path from "node:path";
+import fs from "node:fs";
+
+export const filePathArrayValidator: envalid.StructuredValidator =
+  makeStructuredValidator((value) => {
+    if (value.length === 0) {
+      return [];
+    }
+    return value.split(path.delimiter).flatMap((relativePath) => {
+      if (relativePath.length === 0) {
+        return [];
+      }
+      const absolutePath = path.resolve(relativePath);
+      const stat = fs.statSync(absolutePath);
+      if (stat.isFile()) {
+        return [absolutePath];
+      } else if (stat.isDirectory()) {
+        const filePaths: string[] = [];
+        for (const dirent of fs.readdirSync(absolutePath, {
+          withFileTypes: true,
+        })) {
+          if (!dirent.isFile()) {
+            continue;
+          }
+          filePaths.push(path.resolve(absolutePath, dirent.name));
+        }
+        return filePaths;
+      } else {
+        throw new Error(`${relativePath}`);
+      }
+    });
+  });
+
+export const intValidator = envalid.makeExactValidator<number>(parseInt);
+
+export const languageTagArrayValidator: envalid.StructuredValidator =
+  makeStructuredValidator((value) => {
+    if (value.length === 0) {
+      return [];
+    }
+    return value.split(",").map((subValue) => subValue.trim());
+  });
