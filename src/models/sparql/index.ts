73878d7e00947d67ae3f07ba7ed900e1c5887ded
Minor Gordon <minorg@server.fake>
2024-06-05 11:08:29 -0400

    export more from sparql

--- src/models/sparql/index.ts
+++ src/models/sparql/index.ts
@@ -1,3 +1,5 @@
 export * from "./ConstructQueryBuilder";
 export * from "./GraphPattern";
 export * from "./Kos";
+export * from "./mapResultRowsToCount";
+export * from "./mapResultRowsToIdentifiers";
