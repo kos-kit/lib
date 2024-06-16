eb8311eed37ea35c3421bdeaa0fa4e082a36598f
Minor Gordon <minorg@server.fake>
2024-06-16 16:48:29 -0400

    disperse remainder of client

--- packages/client/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * as models from "./models";
-export * as search from "./search";
-export * from "./SparqlClient";
-export * as utilities from "./utilities";
-export * as vocabularies from "./vocabularies";
