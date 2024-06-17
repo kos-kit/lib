f56584fe9aa31b9b6c08e6c50c8d099591820212
Minor Gordon <minorg@server.fake>
2024-06-17 10:28:17 -0400

    search tests

---
+++ packages/search/__tests__/ServerSearchEngine.test.ts
@@ -0,0 +1,9 @@
+import { ServerSearchEngine } from "../src";
+import { behavesLikeSearchEngine } from "./behavesLikeSearchEngine";
+import { describe } from "vitest";
+
+(process.env["CI"] ? describe.skip : describe)("ServerSearchEngine", () => {
+  behavesLikeSearchEngine(() =>
+    Promise.resolve(new ServerSearchEngine("http://localhost:7878/search")),
+  );
+});
