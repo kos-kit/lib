f567f632321b72886851ec5da19516f5cbd34456
Minor Gordon <minorg@server.fake>
2024-06-23 12:06:12 -0400

    don't export parse

--- packages/next-utils/index.ts
+++ packages/next-utils/index.ts
@@ -2,5 +2,3 @@ export * from "./defilenamify.js";
 export * from "./filenamify.js";
 export * from "./GlobalRef.js";
 export * from "./pageCount.js";
-export * from "./parseRdfFile.js";
-export * from "./parseRdfFiles.js";
