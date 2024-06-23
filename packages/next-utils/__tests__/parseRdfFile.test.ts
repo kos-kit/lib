b182841707f88e2f13e9a7f8e31c881a7a59aa36
Minor Gordon <minorg@server.fake>
2024-06-23 11:44:57 -0400

    parseRdfFile test

---
+++ packages/next-utils/__tests__/parseRdfFile.test.ts
@@ -0,0 +1,25 @@
+import { describe, expect, it } from "vitest";
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { parseRdfFile } from "../parseRdfFile";
+
+describe("parseRdfFile", () => {
+  const testDataDirPath = path.resolve(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "..",
+    "..",
+    "..",
+    "test-data",
+  );
+  for (const fileName of fs.readdirSync(testDataDirPath)) {
+    if (fileName.startsWith("unesco-thesaurus")) {
+      it(`should parse ${fileName}`, async () => {
+        const dataset = await parseRdfFile(
+          path.resolve(testDataDirPath, fileName),
+        );
+        expect(dataset.size).toBe(88482);
+      });
+    }
+  }
+});
