1d2566c377db012d17a3dbc8df5b8afab3a2a127
Minor Gordon <minorg@server.fake>
2024-06-27 18:06:06 -0400

    Model.Identifier

--- packages/models/Model.ts
+++ packages/models/Model.ts
@@ -1,7 +1,8 @@
-import { Literal, NamedNode } from "@rdfjs/types";
+import { BlankNode, Literal, NamedNode } from "@rdfjs/types";
 import { Option } from "fp-ts/Option";
 
 export interface Model {
+  readonly identifier: BlankNode | NamedNode;
   readonly license: Option<Literal | NamedNode>;
   readonly modified: Option<Literal>;
   readonly rights: Option<Literal>;
