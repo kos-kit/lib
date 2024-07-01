18e6acddea2d0aee393b8a43d1dae6e388ef18a1
Minor Gordon <minorg@server.fake>
2024-07-01 11:16:25 -0400

    undo bizarre damage from organization

--- packages/rdf-utils/isInstanceOf.ts
+++ packages/rdf-utils/isInstanceOf.ts
@@ -1,157 +1,81 @@
-// File: /Users/minor/projects/kos-kit/explorer/app/[languageTag]/concepts/[conceptIdentifier]/semanticRelations/[semanticRelationPropertyName]/page.tsx
-import type {
-  ResolvingMetadata,
-  ResolvingViewport,
-} from "next/dist/lib/metadata/types/metadata-interface.js";
-import * as entry from "../../../../../../../../app/[languageTag]/concepts/[conceptIdentifier]/semanticRelations/[semanticRelationPropertyName]/page.js";
+import TermSet from "@rdfjs/term-set";
+import { BlankNode, DatasetCore, NamedNode } from "@rdfjs/types";
+import { rdf, rdfs } from "@tpluscode/rdf-ns-builders";
 
-type TEntry =
-  typeof import("../../../../../../../../app/[languageTag]/concepts/[conceptIdentifier]/semanticRelations/[semanticRelationPropertyName]/page.js");
-
-// Check that the entry is a valid entry
-checkFields<
-  Diff<
-    {
-      default: Function;
-      config?: {};
-      generateStaticParams?: Function;
-      revalidate?: RevalidateRange<TEntry> | false;
-      dynamic?: "auto" | "force-dynamic" | "error" | "force-static";
-      dynamicParams?: boolean;
-      fetchCache?:
-        | "auto"
-        | "force-no-store"
-        | "only-no-store"
-        | "default-no-store"
-        | "default-cache"
-        | "only-cache"
-        | "force-cache";
-      preferredRegion?: "auto" | "global" | "home" | string | string[];
-      runtime?: "nodejs" | "experimental-edge" | "edge";
-      maxDuration?: number;
-
-      metadata?: any;
-      generateMetadata?: Function;
-      viewport?: any;
-      generateViewport?: Function;
-    },
-    TEntry,
-    ""
-  >
->();
-
-// Check the prop type of the entry function
-checkFields<Diff<PageProps, FirstArg<TEntry["default"]>, "default">>();
-
-// Check the arguments and return type of the generateMetadata function
-if ("generateMetadata" in entry) {
-  checkFields<
-    Diff<
-      PageProps,
-      FirstArg<MaybeField<TEntry, "generateMetadata">>,
-      "generateMetadata"
-    >
-  >();
-  checkFields<
-    Diff<
-      ResolvingMetadata,
-      SecondArg<MaybeField<TEntry, "generateMetadata">>,
-      "generateMetadata"
-    >
-  >();
+export function isInstanceOf({
+  class_,
+  instance,
+  dataset,
+  includeSubclasses,
+  instanceOfPredicate,
+  subClassOfPredicate,
+}: {
+  class_: NamedNode;
+  dataset: DatasetCore;
+  includeSubclasses?: boolean;
+  instance: BlankNode | NamedNode;
+  instanceOfPredicate?: NamedNode;
+  subClassOfPredicate?: NamedNode;
+}): boolean {
+  return isInstanceOfRecursive({
+    class_,
+    dataset,
+    includeSubclasses: includeSubclasses ?? true,
+    instance,
+    instanceOfPredicate: instanceOfPredicate ?? rdf.type,
+    subClassOfPredicate: subClassOfPredicate ?? rdfs.subClassOf,
+    visitedClasses: new TermSet<NamedNode>(),
+  });
 }
 
-// Check the arguments and return type of the generateViewport function
-if ("generateViewport" in entry) {
-  checkFields<
-    Diff<
-      PageProps,
-      FirstArg<MaybeField<TEntry, "generateViewport">>,
-      "generateViewport"
-    >
-  >();
-  checkFields<
-    Diff<
-      ResolvingViewport,
-      SecondArg<MaybeField<TEntry, "generateViewport">>,
-      "generateViewport"
-    >
-  >();
+function isInstanceOfRecursive({
+  class_,
+  dataset,
+  includeSubclasses,
+  instance,
+  instanceOfPredicate,
+  subClassOfPredicate,
+  visitedClasses,
+}: {
+  class_: NamedNode;
+  dataset: DatasetCore;
+  includeSubclasses: boolean;
+  instance: BlankNode | NamedNode;
+  instanceOfPredicate: NamedNode;
+  subClassOfPredicate: NamedNode;
+  visitedClasses: TermSet<NamedNode>;
+}): boolean {
+  for (const _ of dataset.match(instance, instanceOfPredicate, class_)) {
+    return true;
+  }
+
+  visitedClasses.add(class_);
+
+  if (!includeSubclasses) {
+    return false;
+  }
+
+  // Recurse into class's sub-classes that haven't been visited yet.
+  for (const quad of dataset.match(null, subClassOfPredicate, class_, null)) {
+    if (quad.subject.termType !== "NamedNode") {
+      continue;
+    } else if (visitedClasses.has(quad.subject)) {
+      continue;
+    }
+    if (
+      isInstanceOfRecursive({
+        class_: quad.subject,
+        dataset,
+        includeSubclasses,
+        instance,
+        instanceOfPredicate,
+        subClassOfPredicate,
+        visitedClasses,
+      })
+    ) {
+      return true;
+    }
+  }
+
+  return false;
 }
-
-// Check the arguments and return type of the generateStaticParams function
-if ("generateStaticParams" in entry) {
-  checkFields<
-    Diff<
-      { params: PageParams },
-      FirstArg<MaybeField<TEntry, "generateStaticParams">>,
-      "generateStaticParams"
-    >
-  >();
-  checkFields<
-    Diff<
-      {
-        __tag__: "generateStaticParams";
-        __return_type__: any[] | Promise<any[]>;
-      },
-      {
-        __tag__: "generateStaticParams";
-        __return_type__: ReturnType<MaybeField<TEntry, "generateStaticParams">>;
-      }
-    >
-  >();
-}
-
-type PageParams = any;
-export interface PageProps {
-  params?: any;
-  searchParams?: any;
-}
-export interface LayoutProps {
-  children?: React.ReactNode;
-  params?: any;
-}
-
-// =============
-// Utility types
-type RevalidateRange<T> = T extends { revalidate: any }
-  ? NonNegative<T["revalidate"]>
-  : never;
-
-// If T is unknown or any, it will be an empty {} type. Otherwise, it will be the same as Omit<T, keyof Base>.
-type OmitWithTag<T, K extends keyof any, _M> = Omit<T, K>;
-type Diff<Base, T extends Base, Message extends string = ""> = 0 extends 1 & T
-  ? {}
-  : OmitWithTag<T, keyof Base, Message>;
-
-type FirstArg<T extends Function> = T extends (...args: [infer T, any]) => any
-  ? unknown extends T
-    ? any
-    : T
-  : never;
-type SecondArg<T extends Function> = T extends (...args: [any, infer T]) => any
-  ? unknown extends T
-    ? any
-    : T
-  : never;
-type MaybeField<T, K extends string> = T extends { [k in K]: infer G }
-  ? G extends Function
-    ? G
-    : never
-  : never;
-
-function checkFields<_ extends { [k in keyof any]: never }>() {}
-
-// https://github.com/sindresorhus/type-fest
-type Numeric = number | bigint;
-type Zero = 0 | 0n;
-type Negative<T extends Numeric> = T extends Zero
-  ? never
-  : `${T}` extends `-${string}`
-    ? T
-    : never;
-type NonNegative<T extends Numeric> = T extends Zero
-  ? T
-  : Negative<T> extends never
-    ? T
-    : "__invalid_negative_number__";
