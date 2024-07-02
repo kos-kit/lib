d07e87b1281a6fedb474c0dd43badf4a942f63e2
Minor Gordon <minorg@server.fake>
2024-07-02 14:31:21 -0400

    envalidValidators don't recurse

--- packages/next-utils/envalidValidators.ts
+++ packages/next-utils/envalidValidators.ts
@@ -2,43 +2,39 @@ import * as envalid from "envalid";
 import fs from "node:fs";
 import path from "node:path";
 
-export const directoryPathValidator = envalid.makeExactValidator((value) => {
+export const existingPathValidator = envalid.makeExactValidator((value) => {
+  if (value.length === 0) {
+    throw new Error("not specified");
+  }
+  const resolvedPath = path.resolve(value);
+  if (!fs.existsSync(resolvedPath)) {
+    throw new Error(`${resolvedPath} does not exist`);
+  }
+  return resolvedPath;
+});
+
+export const existingPathsValidator = envalid.makeExactValidator((value) => {
+  if (value.length === 0) {
+    return [];
+  }
+  return value.split(path.delimiter).flatMap((relativePath) => {
+    if (relativePath.length === 0) {
+      return [];
+    }
+    const resolvedPath = path.resolve(relativePath);
+    if (!fs.existsSync(resolvedPath)) {
+      throw new Error(`${resolvedPath} does not exist`);
+    }
+    return [resolvedPath];
+  });
+});
+
+export const pathValidator = envalid.makeExactValidator((value) => {
   if (value.length === 0) {
     throw new Error("not specified");
   }
   return path.resolve(value);
 });
-
-export const existingFilePathArrayValidator = envalid.makeExactValidator(
-  (value) => {
-    if (value.length === 0) {
-      return [];
-    }
-    return value.split(path.delimiter).flatMap((relativePath) => {
-      if (relativePath.length === 0) {
-        return [];
-      }
-      const absolutePath = path.resolve(relativePath);
-      const stat = fs.statSync(absolutePath);
-      if (stat.isFile()) {
-        return [absolutePath];
-      } else if (stat.isDirectory()) {
-        const filePaths: string[] = [];
-        for (const dirent of fs.readdirSync(absolutePath, {
-          withFileTypes: true,
-        })) {
-          if (!dirent.isFile()) {
-            continue;
-          }
-          filePaths.push(path.resolve(absolutePath, dirent.name));
-        }
-        return filePaths;
-      } else {
-        throw new Error(relativePath);
-      }
-    });
-  },
-);
 
 export const intValidator = envalid.makeExactValidator<number>(parseInt);
 
