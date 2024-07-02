dee2412beaeb0f372225c1a232d7b5ff1a6e1547
Minor Gordon <minorg@server.fake>
2024-07-02 14:39:25 -0400

    remove parseRdfFiles

--- packages/next-utils/index.ts
+++ packages/next-utils/index.ts
@@ -1,4 +1,6 @@
 export * from "./defilenamify.js";
+// Don't add envalidValidators to avoid browser-incompatible imports
 export * from "./filenamify.js";
 export * from "./GlobalRef.js";
 export * from "./pageCount.js";
+// Don't add parseRdfFile to avoid browser-incompatible imports
