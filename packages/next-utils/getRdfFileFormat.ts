aa0c43a2cc8da4c2c4237826fca7c71b69d4e2a9
Minor Gordon <minorg@server.fake>
2024-07-13 16:48:00 -0400

    isRdfFile -> getRdfFileFormat

---
+++ packages/next-utils/getRdfFileFormat.ts
@@ -0,0 +1,58 @@
+import * as path from "node:path";
+import { CompressionMethod, compressionMethods } from "./CompressionMethod.js";
+import { RdfFormat, rdfFormats } from "./RdfFormat.js";
+import { Either, Just, Left, Maybe, Nothing, Right } from "purify-ts";
+import standardMimeTypes from "mime/types/standard.js";
+import otherMimeTypes from "mime/types/other.js";
+import { Mime } from "mime";
+
+const mime = new Mime(standardMimeTypes, otherMimeTypes, {
+  "application/x-brotli": ["br"],
+});
+
+export function getRdfFileFormat(filePath: string): Either<
+  Error,
+  {
+    compressionMethod: Maybe<CompressionMethod>;
+    rdfFormat: RdfFormat;
+  }
+> {
+  const mimeType = mime.getType(filePath);
+  if (mimeType === null) {
+    return Left(new Error("unable to infer MIME type of " + filePath));
+  }
+
+  for (const compressionMethod of compressionMethods) {
+    if (compressionMethod === mimeType) {
+      const uncompressedFileName = path.basename(
+        path.basename(filePath),
+        path.extname(filePath),
+      );
+      const uncompressedMimeType = mime.getType(uncompressedFileName);
+      if (uncompressedMimeType === null) {
+        return Left(
+          new Error("unable to infer MIME type of " + uncompressedFileName),
+        );
+      }
+      for (const rdfFormat of rdfFormats) {
+        if (uncompressedMimeType === rdfFormat) {
+          return Right({
+            compressionMethod: Just(compressionMethod),
+            rdfFormat,
+          });
+        }
+      }
+    }
+  }
+
+  for (const rdfFormat of rdfFormats) {
+    if (mimeType === rdfFormat) {
+      return Right({
+        compressionMethod: Nothing,
+        rdfFormat,
+      });
+    }
+  }
+
+  return Left(new Error(`${filePath} has a non-RDF MIME type: ${mimeType}`));
+}
