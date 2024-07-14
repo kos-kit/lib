d74b4144221cd62a819e363c9abb48a4c3529e56
Minor Gordon <minorg@server.fake>
2024-07-13 21:27:04 -0400

    comment out oxigraph for the time being

--- packages/sparql-models/OxigraphRdfjsCompat.ts
+++ packages/sparql-models/OxigraphRdfjsCompat.ts
@@ -1,45 +1,45 @@
-import * as oxigraph from "oxigraph";
-import * as rdfjs from "@rdfjs/types";
+// import * as oxigraph from "oxigraph";
+// import * as rdfjs from "@rdfjs/types";
 
-export type Quad = oxigraph.Quad;
+// export type Quad = oxigraph.Quad;
 
-export class DatasetCore implements rdfjs.DatasetCore<Quad, Quad> {
-  constructor(private readonly delegate: oxigraph.Store) {}
+// export class DatasetCore implements rdfjs.DatasetCore<Quad, Quad> {
+//   constructor(private readonly delegate: oxigraph.Store) {}
 
-  add(quad: Quad): this {
-    this.delegate.add(quad);
-    return this;
-  }
+//   add(quad: Quad): this {
+//     this.delegate.add(quad);
+//     return this;
+//   }
 
-  delete(quad: Quad): this {
-    this.delegate.delete(quad);
-    return this;
-  }
+//   delete(quad: Quad): this {
+//     this.delegate.delete(quad);
+//     return this;
+//   }
 
-  has(quad: Quad): boolean {
-    return this.delegate.has(quad);
-  }
+//   has(quad: Quad): boolean {
+//     return this.delegate.has(quad);
+//   }
 
-  match(
-    subject?: rdfjs.Term | null,
-    predicate?: rdfjs.Term | null,
-    object?: rdfjs.Term | null,
-    graph?: rdfjs.Term | null,
-  ): DatasetCore {
-    return new DatasetCore(
-      new oxigraph.Store(
-        this.delegate.match(subject, predicate, object, graph),
-      ),
-    );
-  }
+//   match(
+//     subject?: rdfjs.Term | null,
+//     predicate?: rdfjs.Term | null,
+//     object?: rdfjs.Term | null,
+//     graph?: rdfjs.Term | null,
+//   ): DatasetCore {
+//     return new DatasetCore(
+//       new oxigraph.Store(
+//         this.delegate.match(subject, predicate, object, graph),
+//       ),
+//     );
+//   }
 
-  *[Symbol.iterator](): Iterator<Quad> {
-    for (const quad of this.delegate.match(null, null, null, null)) {
-      yield quad as Quad;
-    }
-  }
+//   *[Symbol.iterator](): Iterator<Quad> {
+//     for (const quad of this.delegate.match(null, null, null, null)) {
+//       yield quad as Quad;
+//     }
+//   }
 
-  get size() {
-    return this.delegate.size;
-  }
-}
+//   get size() {
+//     return this.delegate.size;
+//   }
+// }
--- packages/sparql-models/OxigraphSparqlClient.ts
+++ packages/sparql-models/OxigraphSparqlClient.ts
@@ -1,33 +1,33 @@
-import { Store } from "oxigraph";
-import { SparqlClient } from "./SparqlClient.js";
-import { DatasetCore, Quad, Term } from "@rdfjs/types";
-import { ResultRow } from "sparql-http-client/ResultParser.js";
-import * as OxigraphRdfjsCompat from "./OxigraphRdfjsCompat.js";
+// import { Store } from "oxigraph";
+// import { SparqlClient } from "./SparqlClient.js";
+// import { DatasetCore, Quad, Term } from "@rdfjs/types";
+// import { ResultRow } from "sparql-http-client/ResultParser.js";
+// import * as OxigraphRdfjsCompat from "./OxigraphRdfjsCompat.js";
 
-export class OxigraphSparqlClient implements SparqlClient {
-  constructor(private readonly delegate: Store) {}
+// export class OxigraphSparqlClient implements SparqlClient {
+//   constructor(private readonly delegate: Store) {}
 
-  readonly query: SparqlClient.Query = {
-    ask: async (query: string) => this.delegate.query(query) as boolean,
-    construct: async (query: string): Promise<DatasetCore> => {
-      return new OxigraphRdfjsCompat.DatasetCore(
-        new Store(this.delegate.query(query) as Quad[]),
-      );
-    },
-    select: async (query: string): Promise<readonly ResultRow[]> => {
-      const resultRows: ResultRow[] = [];
-      for (const map of this.delegate.query(query) as Map<string, Term>[]) {
-        for (const entry of map.entries()) {
-          const resultRow: Record<string, Term> = {};
-          resultRow[entry[0]] = entry[1];
-          resultRows.push(resultRow);
-        }
-      }
-      return resultRows;
-    },
-  };
+//   readonly query: SparqlClient.Query = {
+//     ask: async (query: string) => this.delegate.query(query) as boolean,
+//     construct: async (query: string): Promise<DatasetCore> => {
+//       return new OxigraphRdfjsCompat.DatasetCore(
+//         new Store(this.delegate.query(query) as Quad[]),
+//       );
+//     },
+//     select: async (query: string): Promise<readonly ResultRow[]> => {
+//       const resultRows: ResultRow[] = [];
+//       for (const map of this.delegate.query(query) as Map<string, Term>[]) {
+//         for (const entry of map.entries()) {
+//           const resultRow: Record<string, Term> = {};
+//           resultRow[entry[0]] = entry[1];
+//           resultRows.push(resultRow);
+//         }
+//       }
+//       return resultRows;
+//     },
+//   };
 
-  readonly update: SparqlClient.Update = {
-    update: async (update: string) => this.delegate.update(update, {}),
-  };
-}
+//   readonly update: SparqlClient.Update = {
+//     update: async (update: string) => this.delegate.update(update, {}),
+//   };
+// }
