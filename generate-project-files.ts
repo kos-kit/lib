b573a7d8f91b190f4f02964f3e0315d19851454e
Minor Gordon <minorg@server.fake>
2024-08-03 16:42:09 -0400

    fix npm publish command

--- generate-project-files.ts
+++ generate-project-files.ts
@@ -1,7 +1,7 @@
 import fs from "node:fs";
 import path from "node:path";
 
-const VERSION = "2.0.35";
+const VERSION = "2.0.36";
 
 type ProjectName =
   | "rdfjs-dataset-models"
@@ -169,7 +169,7 @@ for (const project of projects) {
         },
         type: "module",
         types: "index.d.ts",
-        version: "2.0.35",
+        version: "2.0.36",
       },
       undefined,
       2,
