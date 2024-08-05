e4f61abc873588eaaafc0212963e9c97bb1ff0e1
Minor Gordon <minorg@server.fake>
2024-08-05 18:51:59 -0400

    bump version

--- generate-project-files.ts
+++ generate-project-files.ts
@@ -1,7 +1,7 @@
 import fs from "node:fs";
 import path from "node:path";
 
-const VERSION = "2.0.43";
+const VERSION = "2.0.44";
 
 type ProjectName =
   | "rdfjs-dataset-models"
@@ -174,7 +174,7 @@ for (const project of projects) {
         },
         type: "module",
         types: "index.d.ts",
-        version: "2.0.43",
+        version: "2.0.44",
       },
       undefined,
       2,
