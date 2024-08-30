842823d78fe1a61a183a1cce03795a728e9b98ca
Minor Gordon <minorg@server.fake>
2024-08-30 11:11:43 -0400

    bump version

--- packages/sparql-client/index.ts
+++ packages/sparql-client/index.ts
@@ -4,6 +4,7 @@ export * from "./HttpSparqlUpdateClient.js";
 export * from "./LoggingSparqlGraphStoreClient.js";
 export * from "./LoggingSparqlQueryClient.js";
 export * from "./LoggingSparqlUpdateClient.js";
+export * from "./OxigraphSparqlClient.js";
 export * from "./SparqlGraphStoreClient.js";
 export * from "./SparqlQueryClient.js";
 export * from "./SparqlUpdateClient.js";
