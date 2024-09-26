b78cc1e26f88feea7517ffa011a112b0c7f60aa7
Minor Gordon <minorg@server.fake>
2024-09-26 12:49:55 -0400

    some carnage to get displayLabel off of Model

--- packages/models/LabeledModel.ts
+++ packages/models/LabeledModel.ts
@@ -3,6 +3,8 @@ import { LiteralLabel } from "./LiteralLabel.js";
 import { Model } from "./Model.js";
 
 export interface LabeledModel<LabelT extends Label> extends Model {
+  readonly displayLabel: string;
+
   labels(options?: { types?: readonly Label.Type[] }): readonly (
     | LiteralLabel
     | LabelT
