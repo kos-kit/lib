380bf5ab7205c2ec41f9999b60e3f358c1187481
Minor Gordon <minorg@server.fake>
2024-11-30 18:05:01 -0500

    bump version

--- generate-package-files.ts
+++ generate-package-files.ts
@@ -2,7 +2,7 @@ import fs from "node:fs";
 import path from "node:path";
 import { stringify as stringifyYaml } from "yaml";
 
-const VERSION = "2.0.96";
+const VERSION = "2.0.97";
 
 type PackageName =
   | "rdfjs-dataset-models"
