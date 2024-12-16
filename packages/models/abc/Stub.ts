e80f26911cb7a905dfe6678a46483eb038315111
Minor Gordon <minorg@server.fake>
2024-12-16 15:13:13 -0500

    add cons and hash

--- packages/models/abc/Stub.ts
+++ packages/models/abc/Stub.ts
@@ -4,9 +4,11 @@ import { Equatable } from "purify-ts-helpers";
 import { Identifier } from "../Identifier.js";
 import { Model } from "../Model.js";
 import { Stub as IStub } from "../Stub.js";
+import { StubSequence } from "../StubSequence.js";
 
 export abstract class Stub<ModelT extends Model> implements IStub<ModelT> {
   equals = Stub.equals;
+  hash = Stub.hash;
   abstract readonly identifier: Identifier;
   protected readonly logger: Logger;
 
@@ -18,10 +20,22 @@ export abstract class Stub<ModelT extends Model> implements IStub<ModelT> {
     this.logger = logger;
   }
 
+  abstract cons(...tail: readonly IStub<ModelT>[]): StubSequence<ModelT>;
+
   abstract resolve(): Promise<Either<this, ModelT>>;
 }
 
 export namespace Stub {
+  export function hash<
+    HasherT extends {
+      update: (message: string | number[] | ArrayBuffer | Uint8Array) => void;
+    },
+    ModelT extends Model,
+  >(this: IStub<ModelT>, _hasher: HasherT): HasherT {
+    _hasher.update(Identifier.toString(this.identifier));
+    return _hasher;
+  }
+
   export function equals<ModelT extends Model>(
     this: IStub<ModelT>,
     other: IStub<ModelT>,
